3d3462ef71699d1c9b999337f5ac7a57729f8715
oks2vd <oks2vd@virginia.edu>
2016-03-26 15:10:17 -0400

    add_assignment to main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def home():
     return render_template("main.html")
 
+@app.route("/add_assignment")
+def add_assignment():
+    return render_template("add_assignment.html")
+
 
 if __name__ == "__main__":
     app.debug = True
