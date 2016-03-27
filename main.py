2d1b7191ca9fa76b50694cd1f1130f60717a983f
oks2vd <oks2vd@virginia.edu>
2016-03-26 20:56:26 -0400

    algorithm for caluulating assignment width and height

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 import store
 from objects import Assignment
 from flask import Flask, render_template, make_response
-
-
+from datetime import datetime
 
 app = Flask(__name__)
 pstore = store.PickleStore()
@@ -36,7 +35,25 @@ def show():
     for asgn in pstore.store:
         page += asgn.html()
     print(page)
+    print(assignment_list(pstore, 700, 700))
     return  make_response("<html><head></head><body>" + page + "</body></html>")
+
+def assignment_list(ptstore, canvas_width, canvas_height):
+    #Find distance of farthest assignment
+    till_due_max = max([(asgn.due - datetime.now()).total_seconds()/3600 for asgn in ptstore.store])
+
+    # Creates list of coordinates for creating canvas boxes
+    number_of_assignments = len(ptstore.store)
+    curr_task = 0
+    coordinate_list = []
+    for asgn in ptstore.store:
+            till_due = int( (asgn.due - datetime.now()).total_seconds()/3600)
+            assign_x = int((till_due/till_due_max)*canvas_width)
+            assign_y = int(canvas_height/number_of_assignments)
+            curr_task_coor = [0, curr_task*assign_y, assign_x, assign_y]
+            coordinate_list.append(curr_task_coor)
+            curr_task += 1
+    return coordinate_list
 
 if __name__ == "__main__":
     app.debug = True
